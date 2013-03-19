4297ab5c069aae063373d5dcaf0bd12a941308d9
petryjc <petryj@rose-hulman.edu>
2013-03-18 22:25:31 -0400

    added initial project and documentation

---
+++ Ascension/src/GameTest.java
@@ -0,0 +1,14 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class GameTest {
+
+	@Test
+	public void testInitialization() {
+		Game g = new Game();
+		assertNotNull(g);
+	}
+
+}
